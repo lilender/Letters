d13b8c1c8a0135d9e93d64522e90d5ad26b6d185
Ritzel2086070 <100053852+Ritzel2086070@users.noreply.github.com>
2024-10-08 18:55:43 -0600

    views base
    log in, sign, in, index principal ahahahhahaah creo no cenaré, que hueva cenar

---
+++ JS.js
@@ -0,0 +1,24 @@
+//Loader
+var timeOut;
+function myFunction() {
+    timeOut = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "flex";
+  document.getElementById("myFoot").style.display = "flex";
+}
+
+//-----------------------------------Funciones para desplazarse entre las vistas
+function toSignIn() {
+  window.location.href = 'signin.html';
+}
+
+function toLogIn(){
+  window.location.href = 'login.html';
+}
+
+function toIndex(){
+  window.location.href = 'index.html';
+}
