1884d0a61e07226644bbeb3b7dfcceda83cb0eef
lilender <96185142+lilender@users.noreply.github.com>
2024-10-11 18:34:25 -0600

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/userController');
+const UserController = require('./controllers/userController');
 
 router.post('/login', UserController.login);
 router.post('/register', UserController.register);
