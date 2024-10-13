2230bf05b69674be02928398c28aab178f36bbf4
Ritzel2086070 <100053852+Ritzel2086070@users.noreply.github.com>
2024-10-12 20:42:19 -0600

    ajustes haha

--- public/js/JS.js
+++ public/js/JS.js
@@ -4,6 +4,29 @@ function myFunction() {
     timeOut = setTimeout(showPage, 3000);
 }
 
+// Selecciona todos los elementos con la clase 'nav-bar-chats'
+    const chatElements = document.querySelectorAll('.nav-bar-chats');
+
+    // Función para quitar la clase 'selected' de todos los chats
+    function clearSelection() {
+        chatElements.forEach(chat => {
+            chat.classList.remove('selected');
+        });
+    }
+
+    // Agrega un evento de clic a cada chat
+    chatElements.forEach(chat => {
+        chat.addEventListener('click', () => {
+            // Limpia la selección previa
+            clearSelection();
+            // Agrega la clase 'selected' al chat clickeado
+            chat.classList.add('selected');
+        });
+    });
+
+
+
+
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "flex";
