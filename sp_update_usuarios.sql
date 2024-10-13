88e9e2a33b049260bf42027e6a0a954320e7fc00
lilender <96185142+lilender@users.noreply.github.com>
2024-10-13 16:44:54 -0600

    Update sp_update_usuarios.sql

--- sp_update_usuarios.sql
+++ sp_update_usuarios.sql
@@ -38,7 +38,7 @@ BEGIN
         
         SET @ID_usuario_var = (SELECT u.ID_usuario FROM usuarios u WHERE u.correo = correo);
         
-        SELECT @ID_usuario_var;
+        SELECT (SELECT contrasena_hash FROM usuarios u WHERE u.ID_usuario = (SELECT @ID_usuario_var)) = SHA2(contrasena_hash, 256);
 
 		IF (SELECT contrasena_hash FROM usuarios u WHERE u.ID_usuario = (SELECT @ID_usuario_var)) = SHA2(contrasena_hash, 256) THEN
 			SELECT * from usuarios u WHERE u.ID_usuario = (SELECT @ID_usuario_var);
